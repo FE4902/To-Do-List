fe4676f474cd05c1b3be4cea3d31ccdfb542c90b
FE4902 <isdn000123@gmail.com>
2023-03-08 23:28:42 +0900

    Feat: 할 일 추가하기 기능

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
+import { Item } from "./Item";
+
 export const Body = (): JSX.Element => {
+    const [text, setText] = useState();
+    let todos = localStorage.getItem("todo") as any;
+    let parseToDos = JSON.parse(todos) ?? [];
+
+    const handleChange = (e: any) => {
+        setText(e.target.value);
+    };
+
+    const handleKeyPress = (e: any) => {
+        if (e.key === "Enter") {
+            if (e.target.value) {
+                parseToDos = [...parseToDos, e.target.value];
+                localStorage.setItem("todo", JSON.stringify(parseToDos));
+            }
+            e.target.value = "";
+        }
+    };
+
     return (
         <main className="body">
-            <input className="todo__input" type="text" placeholder="What needs to be done?" />
-            <ul className="todo__list"></ul>
+            <input
+                className="todo__input"
+                type="text"
+                placeholder="What needs to be done?"
+                value={text}
+                onChange={handleChange}
+                onKeyPress={handleKeyPress}
+            />
+            <ul className="todo__list">
+                <Item text="text" />
+                {todos}
+            </ul>
         </main>
     );
 };
